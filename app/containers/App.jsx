35adfaba75b39da37abecf8b41fc753ddfb380c4
Juho Vepsalainen <bebraw@gmail.com>
2017-09-07 13:40:38 +0300

    Initial commit

---
+++ app/containers/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { DragDropContext } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+import Lanes from "../components/Lanes.jsx";
+import { createLane } from "../actions/lanes";
+
+const App = ({ lanes, createLane }) => (
+  <div>
+    <button
+      className="add-lane"
+      onClick={createLane.bind(null, {
+        name: "New lane"
+      })}
+    >
+      +
+    </button>
+    <Lanes lanes={lanes} />
+  </div>
+);
+
+export default compose(
+  connect(
+    state => ({
+      lanes: state.lanes
+    }),
+    {
+      createLane
+    }
+  ),
+  DragDropContext(HTML5Backend)
+)(App);
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const pkg = require("./package.json");
+
+const PATHS = {
+  app: path.join(__dirname, "app"),
+  build: path.join(__dirname, "build")
+};
+
+const commonConfig = {
+  entry: PATHS.app,
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  output: {
+    path: PATHS.build,
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: require("html-webpack-template"),
+      title: "Kanban app",
+      appMountId: "app",
+      inject: false
+    })
+  ]
+};
+
+const developmentConfig = {
+  devtool: "eval-source-map",
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+
+    // display only errors to reduce the amount of output
+    stats: "errors-only",
+
+    // parse host and port from env so this is easy
+    // to customize
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
+  module: {
+    rules: [
+      // Define development specific CSS setup
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
+
+const productionConfig = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[chunkhash].js"
+  },
+  module: {
+    rules: [
+      // Extract CSS during build
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        }),
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    // Output extracted CSS to a file
+    new ExtractTextPlugin("styles.[contenthash].css")
+  ]
+};
+
+module.exports = env => {
+  process.env.BABEL_ENV = env;
+
+  if (env === "development") {
+    return merge(commonConfig, developmentConfig);
+  }
+
+  return merge(commonConfig, productionConfig);
+};

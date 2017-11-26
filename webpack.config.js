300925cf5a14174b73f31e94853e1808e8ef44d0
Juho Vepsalainen <bebraw@gmail.com>
2017-11-26 19:48:14 +0100

    chore: Set up DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -91,6 +91,9 @@ const productionConfig = {
   plugins: [
     // Output extracted CSS to a file
     new ExtractTextPlugin("styles.[contenthash].css"),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": `"production"`,
+    }),
   ],
 };
 
